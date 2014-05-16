7095054eca83061246506dc642f900c573fbc72f
sugardrunk <jannekahkonen@gmail.com>
2014-05-16 12:20:00 +0300

    Fix some stupid errors and add size attribute.

--- links-for-logged/links_for_logged.php
+++ links-for-logged/links_for_logged.php
@@ -7,16 +7,20 @@ Version: 0.1
 Author: Janne Kähkönen
 Author URI: 
 Contributors:
-*/
 
-/*
 Usage:
-    [link_for_logged page="<page>" title="<title>"]
-    [link_for_logged url="<url>" title="<title>"]
+    [link_for_logged page="<page>" title="<title>" size="<p|h1|h2|h3|h4|h5>"]
+    [link_for_logged url="<url>" title="<title>" size="<p|h1|h2|h3|h4|h5>"]
 */
 
 // Tell WP to register the shortcode to the widgets.
 add_filter('widget_text', 'do_shortcode');
+
+// Unregister widget(s).
+function lfl_unregister_default_widgets()
+{
+    unregister_widget('WP_Widget_Text');
+}
 
 // Show or hide the links.
 function lfl_show_links_to_logged($atts)
@@ -28,7 +32,8 @@ function lfl_show_links_to_logged($atts)
          array(
                'page'  => '',
                'url'   => '',
-               'title' => 'Untitled'
+               'title' => 'Untitled',
+               'size'  => 'p'
     ), $atts );
 
     // If any user is logged in.
@@ -37,26 +42,20 @@ function lfl_show_links_to_logged($atts)
         // Return WP page.
         if ($a['page'] !== "")
         {
-            return "<h4 align='center'><a href='" . $WP_PATH .
+            return "<".$a['size']." align='center'><a href='" . $WP_PATH .
                    "/" . $a['page'] . "'>" . $a['title'] .
-                   "</a></h4>";
+                   "</a></".$a['size'].">";
         }
         // Return external URL.
         else if ($a['url'] !== "")
         {
-            return "<h4 align='center'><a href='" . $a['url'] .
-                   "'>" . $a['title'] . "</a></h4>";
+            return "<".$a['size']." align='center'><a href='" . $a['url'] .
+                   "'>" . $a['title'] . "</a></".$a['size'].">";
         }
     }
     else
     {
-        // Unregister widget(s).
-        function lfl_unregister_default_widgets()
-        {
-            unregister_widget('WP_Widget_Text');
-        }
-
-        // Return nothing, remove widget(s).
+        // Return nothing, remove widget.
         add_action('widgets_init', 'lfl_unregister_default_widgets', 0);
     }
 }
